cbdd565dd929d79a5055bafe7b44ba9deae16868
arttupii <arttupii@gmail.com>
2019-07-24 17:21:18 +0300

    Update topic

--- PirSensorNode/PirSensorNode.ino
+++ PirSensorNode/PirSensorNode.ino
@@ -59,7 +59,7 @@ void setup() {
   simpleMqtt.handlePublishEvents([](const char *topic, const char* value) {
   });
 
-  if (!simpleMqtt.publish(deviceName, "/motion/value", "on")) {
+  if (!simpleMqtt.publish(deviceName, "/trigger/pirSensor1/value", "on")) {
     Serial.println("Publish failed... Reboot");
     deepSleepMode(); //Perhaps this works in the next time. Let's go to sleep
   }
