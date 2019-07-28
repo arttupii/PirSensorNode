6db32e58ae90ef761d71a15bb0947bb9aaa2ffca
arttupii <arttupii@gmail.com>
2019-07-29 02:10:58 +0300

    Add 24bit bsid

--- PirSensorNode/PirSensorNode.ino
+++ PirSensorNode/PirSensorNode.ino
@@ -4,6 +4,7 @@
 /********NODE SETUP********/
 #define ESP_NOW_CHANNEL 1
 const char deviceName[] = "PirOlohuone1";
+int bsid = 0x112233;
 //unsigned char secredKey[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
 //unsigned char iv[16] = {0xb2, 0x4b, 0xf2, 0xf7, 0x7a, 0xc5, 0xec, 0x0c, 0x5e, 0x1f, 0x4d, 0xc1, 0xae, 0x46, 0x5e, 0x75};;
 //const int ttl = 3;
@@ -38,7 +39,7 @@ void setup() {
   espNowFloodingMesh_setAesInitializationVector(iv);
   espNowFloodingMesh_setToMasterRole(false, ttl);
   espNowFloodingMesh_setToBatteryNode();
-  espNowFloodingMesh_begin(ESP_NOW_CHANNEL);//, bsid);
+  espNowFloodingMesh_begin(ESP_NOW_CHANNEL, bsid);
 
   espNowFloodingMesh_ErrorDebugCB([](int level, const char *str) {
     Serial.print(level); Serial.println(str); //If you want print some debug prints
